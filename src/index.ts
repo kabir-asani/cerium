02e5e9975e6ed301ecfd842b3cacfdab155fe007
Kabir Asani <iamkabirasani@gmail.com>
2025-03-17 10:31:10 +0530

    Add axios for HTTP requests and implement basic GET/POST functionality

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
-import { serve } from "@hono/node-server";
-import { Hono } from "hono";
+import axios from "axios";
 
-const app = new Hono();
+const baseUrl = "https://a90b05cb-e593-4a77-a500-82b909c678f2.mock.pstmn.io";
 
-serve(app);
+// GET
+try {
+  const response = await axios.post(`${baseUrl}/cities`);
+
+  const statusCode = response.status;
+  const data = response.data;
+
+  console.log("Status: ", statusCode);
+  console.log("Data: ", data);
+} catch (e) {
+  console.log("Error", e);
+}
+
+// POST
+try {
+  const response = await axios.post(`${baseUrl}/cities`, {
+    title: "Buy Fruits",
+  });
+
+  const statusCode = response.status;
+  const data = response.data;
+
+  console.log("Status: ", statusCode);
+  console.log("Data: ", data);
+} catch (e) {
+  console.log("Error", e);
+}
